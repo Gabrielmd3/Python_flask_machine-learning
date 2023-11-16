1c51ca6a5386d6a434933b18b2bc92e026e3c893
Gabriel <gabrielmartinsdelfes3@gmail.com>
2023-11-16 16:43:04 -0300

    trabalho todo

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 from flask import Flask, render_template, request
+import ml
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
+  if request.method == "POST":
+    req = request.form
+    classificador = req["dropdownClassifier"]
+    parametro1 = (req["parametro1"])
+    parametro2 = (req["parametro2"])
+    ml.machine_learning(classificador, parametro1, parametro2)
+    exibir_imagem = True
+    return render_template("index.html", img="displayimg.png", exibir_imagem=exibir_imagem)
   return render_template("index.html")
-
-@app.route("/classificador2")
-def classificador2():
-  return render_template("classificador2.html")
-
-@app.route("/classificador3")
-def classificador3():
-  return render_template("classificador3.html")
-
-@app.route("/classificador4")
-def classificador4():
-  return render_template("classificador4.html")
-
 
 if __name__ == "__main__":
   app.run(debug=True)
